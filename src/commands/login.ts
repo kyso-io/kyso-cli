eedc66480a0240722d0bf8e7bd0d0640d2e13684
Fran Barrena <francisco@kyso.io>
2022-03-02 18:23:53 +0100

    trying to identify WHO IS NEVER

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -116,7 +116,11 @@ export default class Login extends KysoCommand {
       }
     }
 
-    await store.dispatch(loginAction(loginModel) as any)
+    await store.dispatch(
+      loginAction(
+        loginModel
+      )
+    )
     const { auth } = store.getState()
     if (auth.token) {
       this.saveToken(auth.token, flags.organization || null, flags.team || null)
