6f514c28406ba49ee265f190119a27ab1c15c36e
Fran Barrena <francisco@kyso.io>
2022-09-05 16:54:16 +0200

    hardened process

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -12,7 +12,7 @@ export default class Import extends KysoCommand {
   static description = 'Import report into kyso from different sources'
 
   static examples = [
-    `$ kyso import --type powerpoint --path . --mappings Company:organization;Author:author;Keywords:team;Title:title;Comments:description`
+    `$ kyso import --type powerpoint --path . --mappings Company:organization,Author:author,Keywords:team,Title:title,Comments:description`
   ]
 
   static flags = {
@@ -33,7 +33,7 @@ export default class Import extends KysoCommand {
       char: 'm',
       description: 'mappings',
       required: false,
-      default: "Company:organization;Creator:author;Keywords:team;Title:title;Comments:description"
+      default: "Company:organization,Creator:author,Keywords:team,Title:title,Comments:description"
     }),
     verbose: Flags.boolean({
       char: 'x',
@@ -75,152 +75,160 @@ export default class Import extends KysoCommand {
         if (stat.isDirectory()) {
             this.processFolder(filename, extension, flags); //recurse
         } else if (filename.endsWith(extension)) {
-            console.log('🔎 Found: ', filename);
-            const zip: AdmZip = new AdmZip(filename);
-            const zipEntries = zip.getEntries();
+            try {
+              console.log('🔎 Found: ', filename);
+              const zip: AdmZip = new AdmZip(filename);
+              const zipEntries = zip.getEntries();
 
-            const app = zipEntries.filter(x => x.entryName === 'docProps/app.xml');
-            const core = zipEntries.filter(x => x.entryName === 'docProps/core.xml');
-            const thumbnail = zipEntries.filter(x => x.entryName === 'docProps/thumbnail.jpeg');
+              const app = zipEntries.filter(x => x.entryName === 'docProps/app.xml');
+              const core = zipEntries.filter(x => x.entryName === 'docProps/core.xml');
+              const thumbnail = zipEntries.filter(x => x.entryName === 'docProps/thumbnail.jpeg');
 
-            const appJson = JSON.parse(convert.xml2json(app[0].getData().toString()));
-            const coreJson = JSON.parse(convert.xml2json(core[0].getData().toString()));
+              const appJson = JSON.parse(convert.xml2json(app[0].getData().toString()));
+              const coreJson = JSON.parse(convert.xml2json(core[0].getData().toString()));
 
-            const appElements = appJson.elements.map(x => x.elements).flat();
-            const coreElements = coreJson.elements.map(x => x.elements).flat();
+              const appElements = appJson.elements.map(x => x.elements).flat();
+              const coreElements = coreJson.elements.map(x => x.elements).flat();
 
-            const allMetadata = [...coreElements, ...appElements];
+              const allMetadata = [...coreElements, ...appElements];
 
-            let keysFound = 0;
-            const mappingObject = new Map();
+              let keysFound = 0;
+              const mappingObject = new Map();
 
-            for(const metadata of allMetadata) {
-              const mappingData = flags.mappings.split(";");
+              for(const metadata of allMetadata) {
+                const mappingData = flags.mappings.split(",");
 
-              for(const mData of mappingData) {
-                const splittedMap = mData.split(":");
+                for(const mData of mappingData) {
+                  const splittedMap = mData.split(":");
 
-                if(!mappingObject.has(splittedMap[0])) {
-                  mappingObject.set(splittedMap[0], { kyso: splittedMap[1], value: undefined });
+                  if(!mappingObject.has(splittedMap[0])) {
+                    mappingObject.set(splittedMap[0], { kyso: splittedMap[1], value: undefined });
+                  }
                 }
-              }
 
-              for(const k of mappingObject.keys()) {
-                if(metadata.name.toLowerCase().includes(k.toLowerCase())) {
-                  // Match
-                  if(metadata.elements) {
-                    if(metadata.elements[0].type === "text") {
-                      keysFound++;
-                      console.log(`\t${keysFound}/${mappingObject.size} - ${mappingObject.get(k).kyso} is ${metadata.elements[0].text}`);
-                      const data = mappingObject.get(k);
+                for(const k of mappingObject.keys()) {
+                  if(metadata.name.toLowerCase().includes(k.toLowerCase())) {
+                    // Match
+                    if(metadata.elements) {
+                      if(metadata.elements[0].type === "text") {
+                        keysFound++;
+                        console.log(`\t${keysFound}/${mappingObject.size} - ${mappingObject.get(k).kyso} is ${metadata.elements[0].text}`);
+                        const data = mappingObject.get(k);
 
-                      data.value = metadata.elements[0].text;
-                      mappingObject.set(k, data);
+                        data.value = metadata.elements[0].text;
+                        mappingObject.set(k, data);
+                      }
                     }
                   }
                 }
               }
-            }
 
-            let organizationSet = false;
-            let organizationValue = "";
-            let authorSet = false;
-            let authorValue = "";
-            let teamSet = false;
-            let teamValue = "";
-            let titleSet = false;
-            let titleValue = "";
+              let organizationSet = false;
+              let organizationValue = "";
+              let authorSet = false;
+              let authorValue = "";
+              let teamSet = false;
+              let teamValue = "";
+              let titleSet = false;
+              let titleValue = "";
 
-            // Check if minimum data is retrieved
-            for (const [_key, value] of mappingObject) {
-              switch(value.kyso) {
-                case "organization":
-                  if(value.value) {
-                    organizationSet = true;
-                    organizationValue = value.value;
-                  }
-                  break;
-                case "author":
-                  if(value.value) {
-                    authorSet = true;
-                    authorValue = value.value;
-                  }
-                  break;
-                case "team":
-                  if(value.value) {
-                    teamSet = true;
-                    teamValue = value.value;
-                  }
-                  break;
-                case "title":
-                  if(value.value) {
-                    titleSet = true;
-                    titleValue = value.value;
-                  }
-                  break;
-              }
-            }
-
-            // Set defaults if are defined and no-metadata was collected
-            if(!organizationSet && flags.organization) {
-              console.log(`\t🔶 Organization was not found in the document's metadata. Setting default value ${flags.organization}.`)
-              organizationSet = true;
-              organizationValue = flags.organization
-            }
-
-            if(!authorSet && flags.author) {
-              console.log(`\t🔶 Author was not found in the document's metadata. Setting default value ${flags.author}.`)
-              authorSet = true;
-              authorValue = flags.author;
-            }
-
-            if(!teamSet && flags.channel) {
-              console.log(`\t🔶 Channel was not found in the document's metadata. Setting default value ${flags.channel}.`)
-              teamSet = true;
-              teamValue = flags.channel;
-            }
-            const onlyName = filename.replace(/^.*[\\\/]/, '');
-
-            if(!titleSet) {
-              const defaultTitle = onlyName.replace("-", " ").replace("_", " ");
-              console.log(`\t🔶 Title was not found in the document's metadata. Setting the file name as title: ${defaultTitle}.`)
-              titleSet = true;
-              titleValue = defaultTitle;
-            }
-
-            if(organizationSet && authorSet && teamSet && titleSet) {
-              // All right, upload it
-              console.log("\t💚 All right! Uploading report...");
-
-              const random = uuidv4();
-              const tmpFolder = join(KysoCommand.DATA_DIRECTORY, "tmp", random);
-              
-              mkdirSync(tmpFolder);
-              
-              // Copy file to a temporary folder
-              
-              const copyPath = join(tmpFolder, onlyName)
-              copyFileSync(filename, copyPath);
-
-              // Create kyso.json
-              const kysoJson = {
-                main: onlyName,
-                organization: organizationValue,
-                team: teamValue, 
-                authors: [authorValue],
-                title: titleValue,
-                description: "",
-                type: "other"
+              // Check if minimum data is retrieved
+              for (const [_key, value] of mappingObject) {
+                switch(value.kyso) {
+                  case "organization":
+                    if(value.value) {
+                      organizationSet = true;
+                      organizationValue = value.value;
+                    }
+                    break;
+                  case "author":
+                    if(value.value) {
+                      authorSet = true;
+                      authorValue = value.value;
+                    }
+                    break;
+                  case "team":
+                    if(value.value) {
+                      teamSet = true;
+                      teamValue = value.value;
+                    }
+                    break;
+                  case "title":
+                    if(value.value) {
+                      titleSet = true;
+                      titleValue = value.value;
+                    }
+                    break;
+                }
               }
 
-              writeFileSync(`${tmpFolder}/kyso.json`, JSON.stringify(kysoJson));
+              // Set defaults if are defined and no-metadata was collected
+              if(!organizationSet && flags.organization) {
+                console.log(`\t🔶 Organization was not found in the document's metadata. Setting default value ${flags.organization}.`)
+                organizationSet = true;
+                organizationValue = flags.organization
+              }
 
-              // Call kyso push
-              const result = execSync(`kyso push --path ${tmpFolder}`);
-              rmSync(tmpFolder, {recursive: true, force: true});
-              result.toString().split("\n").forEach(x => console.log(`\t${x}`));
-            } else {
-              console.log("\t💔 Sorry, we can't retrieve all the required metadata to upload this report");
+              if(!authorSet && flags.author) {
+                console.log(`\t🔶 Author was not found in the document's metadata. Setting default value ${flags.author}.`)
+                authorSet = true;
+                authorValue = flags.author;
+              }
+
+              if(!teamSet && flags.channel) {
+                console.log(`\t🔶 Channel was not found in the document's metadata. Setting default value ${flags.channel}.`)
+                teamSet = true;
+                teamValue = flags.channel;
+              }
+              const onlyName = filename.replace(/^.*[\\\/]/, '');
+
+              if(!titleSet) {
+                const defaultTitle = onlyName.replace("-", " ").replace("_", " ");
+                console.log(`\t🔶 Title was not found in the document's metadata. Setting the file name as title: ${defaultTitle}.`)
+                titleSet = true;
+                titleValue = defaultTitle;
+              }
+
+              if(organizationSet && authorSet && teamSet && titleSet) {
+                // All right, upload it
+                console.log("\t💚 All right! Uploading report...");
+
+                const random = uuidv4();
+                const tmpFolder = join(KysoCommand.DATA_DIRECTORY, "tmp", random);
+                
+                mkdirSync(tmpFolder);
+                
+                // Copy file to a temporary folder
+                
+                const copyPath = join(tmpFolder, onlyName)
+                copyFileSync(filename, copyPath);
+
+                // Create kyso.json
+                const kysoJson = {
+                  main: onlyName,
+                  organization: organizationValue,
+                  team: teamValue, 
+                  authors: [authorValue],
+                  title: titleValue,
+                  description: "",
+                  type: "other"
+                }
+
+                writeFileSync(`${tmpFolder}/kyso.json`, JSON.stringify(kysoJson));
+
+                // Call kyso push
+                const result = execSync(`kyso push --path ${tmpFolder}`);
+                rmSync(tmpFolder, {recursive: true, force: true});
+                result.toString().split("\n").forEach(x => console.log(`\t${x}`));
+              } else {
+                console.log("\t💔 Sorry, we can't retrieve all the required metadata to upload this report");
+              }
+            } catch (ex) {
+              console.log("🔴 An unexpected error happened when processing " + filename);
+              
+              if(flags.verbose) {
+                console.log(ex);
+              }
             }
         };
     };
