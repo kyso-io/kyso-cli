5c535ca809acb8b81974cd5030bd2c7b2124029d
Fran Barrena <francisco@kyso.io>
2023-03-09 13:50:05 +0100

    feat: new add-member to organization command

---
+++ src/commands/organization/add-member.ts
@@ -0,0 +1,90 @@
+import { KysoRole, UserDTO, OrganizationMember, NormalizedResponseDTO, Organization, AddUserOrganizationDto } from '@kyso-io/kyso-model';
+import { Api } from '@kyso-io/kyso-store';
+import { launchInteractiveLoginIfNotLogged } from '../../helpers/interactive-login';
+import { KysoCredentials } from '../../types/kyso-credentials';
+import { KysoCommand } from '../kyso-command';
+
+export default class AddMemberToOrganization extends KysoCommand {
+  static description = 'Add list_of_user_email users as member to organization_name with the desired role.';
+
+  static examples = [
+    `# Usage`,
+    `$ kyso organization add-member <list_of_user_email> <organization_name> <role>`,
+    `# Add users with default role (team-reader)`,
+    `$ kyso organization add-member lo+rey@dev.kyso.io,lo+palpatine@dev.kyso.io darkside`,
+    `# Add users with specific role`,
+    `$ kyso organization add-member lo+rey@dev.kyso.io,lo+palpatine@dev.kyso.io darkside organization-admin`,
+  ];
+
+  static args = [
+    {
+      name: 'list_of_user_email',
+      description: 'Emails of the users to be added to the organization separated by commas.\nExample: lo+rey@dev.kyso.io,lo+palpatine@dev.kyso.io',
+      required: true,
+    },
+    {
+      name: 'organization_name',
+      description: 'Organization slug name in which the user would be added.\nExample: darkside',
+      required: true,
+    },
+    {
+      name: 'role',
+      description: '<optional argument> Default is team-reader.\nExample: organization-admin',
+      required: false,
+      options: ['team-reader', 'team-contributor', 'team-admin', 'organization-admin'],
+    },
+  ];
+
+  private async addUserToOrganization(api: Api, userEmail: string, organizationSlug: string, role?: string): Promise<void> {
+    try {
+      const kysoCredentials: KysoCredentials = KysoCommand.getCredentials();
+
+      const organizationObject: NormalizedResponseDTO<Organization> = await api.getOrganizationBySlug(organizationSlug);
+
+      if (!organizationObject || !organizationObject.data) {
+        this.error(`Provided organization ${organizationSlug} not found at ${kysoCredentials.kysoInstallUrl}`);
+      }
+
+      const usersResponse: NormalizedResponseDTO<UserDTO[]> = await api.getUsers({ userIds: [], page: 1, per_page: 1000, sort: 'email', search: encodeURIComponent(userEmail) });
+
+      if (!usersResponse || !usersResponse.data || usersResponse.data.length === 0) {
+        this.error(`Provided user ${userEmail} not found at ${kysoCredentials.kysoInstallUrl}`);
+      }
+
+      const newMember: AddUserOrganizationDto = new AddUserOrganizationDto(organizationObject.data.id, usersResponse.data[0].id, role ? role : 'team-reader');
+
+      api.setOrganizationSlug(organizationObject.data.sluglified_name);
+      await api.addUserToOrganization(newMember);
+
+      this.log(`\n${usersResponse.data[0].display_name} added to organization ${organizationObject.data.display_name} at ${kysoCredentials.kysoInstallUrl}\n`);
+    } catch (e: any) {
+      if (!e.response || !e.response.data || !e.response.data.message) {
+        this.log(`Error adding ${userEmail} to ${organizationSlug}: ${e.message}`);
+      } else {
+        this.log(`Error adding ${userEmail} to ${organizationSlug}: ${e.response.data.message}`);
+      }
+    }
+  }
+
+  async run(): Promise<void> {
+    const { args } = await this.parse(AddMemberToOrganization);
+    const list_of_users: string[] = args.list_of_user_email.split(',');
+    const organizationName: string = args.organization_name;
+    const desiredRole: string = args.role;
+
+    await launchInteractiveLoginIfNotLogged();
+    const kysoCredentials: KysoCredentials = KysoCommand.getCredentials();
+
+    const api: Api = new Api();
+    api.configure(kysoCredentials.kysoInstallUrl + '/api/v1', kysoCredentials?.token);
+
+    for (const userEmail of list_of_users) {
+      try {
+        await this.addUserToOrganization(api, userEmail, organizationName, desiredRole);
+      } catch (e) {
+        this.log(`Error adding user ${userEmail} to org ${organizationName} with role ${desiredRole}`);
+        console.log(e);
+      }
+    }
+  }
+}
