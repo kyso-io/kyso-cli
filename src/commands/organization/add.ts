6945ffe337b74058ba5b3d74918aa5c214a99495
Daniel Andújar <daniandujar@gmail.com>
2023-07-17 15:54:32 +0200

    feat: check ONLY_GLOBAL_ADMINS_CAN_CREATE_ORGANIZATIONS variable to allow the creation of organizations

--- src/commands/organization/add.ts
+++ src/commands/organization/add.ts
@@ -1,10 +1,12 @@
-import type { NormalizedResponseDTO, Organization } from '@kyso-io/kyso-model';
-import { AllowDownload, CreateOrganizationDto } from '@kyso-io/kyso-model';
+import type { NormalizedResponseDTO, Organization, TokenPermissions } from '@kyso-io/kyso-model';
+import { AllowDownload, CreateOrganizationDto, KysoSettingsEnum } from '@kyso-io/kyso-model';
 import { Api } from '@kyso-io/kyso-store';
-import inquirer = require('inquirer');
+import jwtDecode from 'jwt-decode';
+import { Helper } from '../../helpers/helper';
 import { launchInteractiveLoginIfNotLogged } from '../../helpers/interactive-login';
 import type { KysoCredentials } from '../../types/kyso-credentials';
 import { KysoCommand } from '../kyso-command';
+import inquirer = require('inquirer');
 
 export default class AddOrganizations extends KysoCommand {
   static description =
@@ -81,6 +83,27 @@ export default class AddOrganizations extends KysoCommand {
     const kysoCredentials: KysoCredentials = KysoCommand.getCredentials();
     const api: Api = new Api();
     api.configure(`${kysoCredentials.kysoInstallUrl}/api/v1`, kysoCredentials?.token);
+    try {
+      const value: NormalizedResponseDTO<any> = await api.getSettingValue(KysoSettingsEnum.ONLY_GLOBAL_ADMINS_CAN_CREATE_ORGANIZATIONS);
+      if (value.data === 'true' || value.data === true) {
+        const decoded: { payload: any } = jwtDecode(kysoCredentials.token);
+        try {
+          const resultPermissions: NormalizedResponseDTO<TokenPermissions> = await api.getUserPermissions(decoded.payload.username);
+          const tokenPermissions: TokenPermissions = resultPermissions.data;
+          const isGlobalAdmin: boolean = Helper.isGlobalAdmin(tokenPermissions);
+          if (!isGlobalAdmin) {
+            this.log('You are not allowed to create organizations. Please contact your system administrator.');
+            return;
+          }
+        } catch (e) {
+          this.log('Error getting user permissions');
+          return;
+        }
+      }
+    } catch (e) {
+      this.log('You are not allowed to create organizations. Please contact your system administrator.');
+      return;
+    }
     for (const organizationDisplayName of organizationsNames) {
       await this.createOrganization(api, organizationDisplayName);
     }
